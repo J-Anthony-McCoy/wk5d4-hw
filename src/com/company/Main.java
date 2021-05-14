dad1a834a8c34944c4ec0a02ab796aa369841c72
Anthony McCoy <james.anthony.mccoy@gmail.com>
2021-05-13 19:25:48 -0500

    first commit to get files on GitHub

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+//make a bunch of errors
+    //initialize means to give a value
+    public static void main(String[] args) {
+	int someName;
+
+	    String String = "Number";
+        System.out.printf(someName);
+        System.out.println(Number);
+
+        int String = "number";
+    }
+}
