a2dd197967043a8af29263776d99e0fabae71316
Anthony McCoy <james.anthony.mccoy@gmail.com>
2021-05-13 19:40:43 -0500

    hw assignment done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,14 +2,35 @@ package com.company;
 
 public class Main {
 //make a bunch of errors
-    //initialize means to give a value
     public static void main(String[] args) {
 	int someName;
 
-	    String String = "Number";
-        System.out.printf(someName);
-        System.out.println(Number);
+	    String String = 7; // wrong variable declaration type
+        System.out.printf(someName); // 'not initialized error'. someName is declared as an int in error
+        System.out.println(Number); // 'Number' not declared
 
-        int String = "number";
+
+
+        int int = "I am text"; // no identifier present
+
+        public void someThing(int someThing) { // variable already defined
+            if (someThing == 4) {
+                System.out.println("Something is wrong");
+            }else {
+                System.out.println("Something is also wrong");
+            }
+        }
+
+        public static void SomeConstructor(String word, int number) {
+            this.word = word;
+            this.number = word; // wrong 'this' assignment
+        }
+
+        boolean results = 19; // provided int when boolean was needed
+
+        int sum = 5 + 5;
+
+        System.out.println(Sum); // capitalized Sum when needed to be lowercase
+
     }
 }
